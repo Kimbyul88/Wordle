cfd747b3ad70b34e99fa80c2b4dc9fe8b15a9650
Kimbyul88 <byul88byul@gmail.com>
2024-01-02 13:54:09 +0900

    여러 오류 수정, 키보드 클릭가능
    휴..

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ function appStart() {
     const div = document.createElement("div");
     div.innerText = "게임이 종료됐습니다.";
     div.style =
-      "display:flex; justify-content:center; align-items:center;position:absolute;top:40vh;left:38vw;width:200px;height:100px;background-color:white";
+      "display:flex; justify-content:center; align-items:center;position:absolute;top:40vh;left:50%;transform:translate(-50%,0%);width:200px;height:100px;background-color:tomato;font-weight:bold;";
     document.body.appendChild(div);
   };
   const gameOver = () => {
@@ -40,15 +40,27 @@ function appStart() {
       const block = document.querySelector(
         `.board-block[data-index='${attempts}${i}']`
       );
+
       const 입력한글자 = block.innerText;
       const 정답글자 = 정답[i];
+      const key = document.querySelector(
+        `.key-block[data-key='${입력한글자}']`
+      );
       if (입력한글자 === 정답글자) {
         맞은개수++;
+        key.style.background = "RGB(106, 170, 100)";
         block.style.background = "RGB(106, 170, 100)";
-      } else if (정답.includes(입력한글자))
+      } else if (정답.includes(입력한글자)) {
         block.style.background = "RGB(201, 180, 88)";
-      else block.style.background = "RGB(120,124,126)";
+        if (key.style.background !== "rgb(106, 170, 100)") {
+          key.style.background = "RGB(201, 180, 88)";
+        }
+      } else {
+        block.style.background = "RGB(120,124,126)";
+        key.style.background = "RGB(120,124,126)";
+      }
       block.style.color = "white";
+      key.style.color = "white";
     }
     if (맞은개수 === 5) gameOver();
     nextLine();
@@ -60,16 +72,65 @@ function appStart() {
     const thisBlock = document.querySelector(
       `.board-block[data-index='${attempts}${index}']`
     );
+    console.log(attempts + "/" + index + "/" + thisBlock);
     if (event.key === "Backspace") handleBackspace();
     else if (index === 5) {
       if (event.key === "Enter") handleEnterKey();
       else return;
     } else if (keyCode >= 65 && keyCode <= 90) {
+      thisBlock.innerText = key;
+      index += 1;
+    }
+  };
+  //const 키배열 = document.querySelectorAll(".key-block");
+  //const 클릭된키 = 키배열[1];
+  const handleMousedown = (event) => {
+    const 알파벳들 = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+    const key = event.target.innerText;
+    console.log("clicked:" + key, typeof key);
+    const thisBlock = document.querySelector(
+      `.board-block[data-index='${attempts}${index}']`
+    );
+    if (key === "BACK") handleBackspace();
+    else if (index === 5) {
+      if (key === "ENTER") handleEnterKey();
+      else return;
+    } else if (key === null) {
+      return;
+    } else if (알파벳들.includes(key)) {
       console.log(attempts + "/" + index);
       thisBlock.innerText = key;
       index += 1;
     }
   };
+
   const startTimer = () => {
     const 시작시간 = new Date();
 
@@ -84,7 +145,9 @@ function appStart() {
 
     timer = setInterval(setTime, 1000);
   };
+
   startTimer();
+  document.addEventListener("mousedown", handleMousedown);
   window.addEventListener("keydown", handleKeydown);
 }
 
